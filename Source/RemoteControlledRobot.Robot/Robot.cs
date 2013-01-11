c341fc4b43dc17708d332d5a7b0070862f9c2828
Jakub Sikorski <jakubsikorski7@gmail.com>
2013-01-11 21:18:01 +0100

    Detecting obstacles

--- Source/RemoteControlledRobot.Robot/Robot.cs
+++ Source/RemoteControlledRobot.Robot/Robot.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using GHIElectronics.NETMF.FEZ;
 using Microsoft.SPOT.Hardware;
+using RemoteControlledRobot.Robot.Components;
+using RemoteControlledRobot.Robot.Utils;
 using RemoteControlledRobot.RobotApi;
 
 namespace RemoteControlledRobot.Robot
@@ -11,12 +13,22 @@ namespace RemoteControlledRobot.Robot
         private static readonly OutputPort Led = new OutputPort((Cpu.Pin)FEZ_Pin.Digital.Di4, true);
         private readonly RobotEventAggregator _robotEventAggregator;
 
+        private const int SensorsTriggerPercentage = 35;
+        private static readonly FEZ_Components.ReflectiveSensor LeftSensor =
+            new FEZ_Components.ReflectiveSensor(FEZ_Pin.AnalogIn.An7, SensorsTriggerPercentage); 
+        private static readonly FEZ_Components.ReflectiveSensor RightSensor = 
+            new FEZ_Components.ReflectiveSensor(FEZ_Pin.AnalogIn.An6, SensorsTriggerPercentage);
+
+        private const int SensorsTimerInterval = 20;
+        private readonly SeparateThreadTimer _reflectiveSensorsTimer;
+
         private int _currentSpeed;
         private float _currentLeft = 1.0f;
         private float _currentRight = 1.0f;
 
         public Robot(RobotEventAggregator robotEventAggregator)
         {
+            _reflectiveSensorsTimer = new SeparateThreadTimer(CheckSensors, SensorsTimerInterval);
             _robotEventAggregator = robotEventAggregator;
         }
 
@@ -25,6 +37,8 @@ namespace RemoteControlledRobot.Robot
             _robotEventAggregator.OnSpeedUpdated += UpdateSpeed;
             _robotEventAggregator.OnDirectionUpdated += UpdateDirection;
             _robotEventAggregator.OnBeep += Beep;
+
+            _reflectiveSensorsTimer.Start();
 
             SignalStarted();
         }
@@ -63,6 +77,20 @@ namespace RemoteControlledRobot.Robot
         private void Beep()
         {
             RobotPiezoController.Beep(DefaultPiezzoBeepDuration);
+        }
+
+        private void CheckSensors()
+        {
+            if (_currentSpeed <= 0)
+                return;
+
+            if (LeftSensor.GetState() == FEZ_Components.ReflectiveSensor.DetectingState.ReflectionDeteced ||
+                RightSensor.GetState() == FEZ_Components.ReflectiveSensor.DetectingState.ReflectionDeteced)
+            {
+                _currentSpeed = 0;
+                UpdateMovement();
+                RobotPiezoController.Beep(1000);
+            }
         }
 
         private static void SignalStarted()
---
+++ Source/RemoteControlledRobot.Robot/Utils/SeparateThreadTimer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using EmbeddedMelodyPlayer.Utils;
+
+namespace RemoteControlledRobot.Robot.Utils
+{
+    public class SeparateThreadTimer
+    {
+        private readonly int _interval;
+        private readonly BasicAction _onTick;
+
+        private bool _isStarted;
+        private bool _shouldWork;
+
+        public SeparateThreadTimer(BasicAction onTick, int interval)
+        {
+            _onTick = onTick;
+            _interval = interval;
+        }
+
+        public void Start()
+        {
+            if (_isStarted)
+                throw new Exception("Cannot start timer that is already started.");
+
+            _shouldWork = true;
+            var workingThread = new Thread(Work);
+            workingThread.Start();
+
+            _isStarted = true;
+        }
+
+        public void Abort()
+        {
+            if (!_isStarted)
+                throw new Exception("Cannot stop not started timer.");
+
+            _shouldWork = false;
+        }
+
+        private void Work()
+        {
+            while (_shouldWork)
+            {
+                _onTick();
+                Thread.Sleep(_interval);
+            }
+        }
+    }
+}
